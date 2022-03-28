992b319f05286c29242ff1270e4df53f073e3d12
F1L1P <mlodzik.filip@wp.pl>
2022-03-28 18:44:41 +0200

    probably last update
    tweaked some error handling in time.py

--- time.py
+++ time.py
@@ -4,13 +4,16 @@ import json
 
 def job():
     print(time.strftime("%H:%M:%S"))
-    if time.strftime("%H:%M:%S")== '18:16:00':
+    if time.strftime("%H:%M:%S")== '00:00:00':
         print("yay")
         with open("list.json","r") as f:
             json_data = json.loads(f.read())
         
         for player in json_data.keys():
             json_data[player]["energy"]+=1
+
+        if len(json_data) == 0:
+            return
 
         with open("list.json","w") as f:
             new_json = json.dumps(json_data, indent=4)
